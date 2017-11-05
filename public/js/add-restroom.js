a825ac6951a5d48fb165b484ce527ed2d9c80577
Alison Lawyer <alison@Alisons-Air.att.net>
2017-11-05 09:04:02 -0800

    saving changes

--- public/js/add-restroom.js
+++ public/js/add-restroom.js
@@ -1,25 +1,29 @@
-$(document).on('click', '#submit', submitRestroom);
+$(document).ready(function () {
 
-var $name = $('#location-name');
-var $location = $('#address');
-var $type = $('label.radio');
-var $comments = $('#comment');
-
-function submitRestroom () {
-  event.preventDefault();
-  var newRestroom = {
-    name: $name.val().trim(),
-    location: $location.val().trim(),
-    category: $type.val(),
-    comments: $comments.val().trim()
-    // createdAt: req.body.createdAt,
-    // updatedAt: req.body.updatedAt
-  };
-  $.post('api/new/restroom', newRestroom, function () {
-    alert('Restroom added!');
-  });
-  $name.val('');
-  $location.val('');
-  $type.val('');
-  $comments.val('');
-}
+  var $name = $('#location-name');
+  var $location = $('#address');
+  var $type = $('label.radio');
+  var $comments = $('#comment');
+  console.log($name + ' on ready');
+  $(document).on('click', '#submit', submitRestroom);
+  function submitRestroom (event) {
+    console.log($name + ' on submit');
+    event.preventDefault();
+    var newRestroom = {
+      name: $name.val().trim(),
+      location: $location.val().trim(),
+      category: $type.val(),
+      comments: $comments.val().trim()
+      // createdAt: req.body.createdAt,
+      // updatedAt: req.body.updatedAt
+    };
+    console.log(newRestroom);
+    $.post('/api/new/restroom', newRestroom, function () {
+      alert('Restroom added!');
+    });
+    $name.val('');
+    $location.val('');
+    $type.val('');
+    $comments.val('');
+  }
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,28 +2,32 @@
 var db = require('../models');
 
 module.exports = function (app) {
-  app.get('api/restrooms', function (req, res) {
+  app.get('/api/restrooms', function (req, res) {
     db.restroom.findAll({})
     .then(function (data) {
       res.json(data);
     });
   });
 
-  app.post('api/new/restroom', function (req, res) {
+  app.post('/api/new/restroom', function (req, res) {
     console.log(req.body);
-    db.restroom.create({
-      name: req.body.name,
-      location: req.body.location,
-      category: req.body.category,
-      comments: req.body.comments,
-      createdAt: req.body.createdAt,
-      updatedAt: req.body.updatedAt
-    })
+    db.restroom.create(
+      req.body
+      // {
+      //   name: req.body.name,
+      //   location: req.body.location,
+      //   category: req.body.category,
+      //   comments: req.body.comments
+      //   // createdAt: req.body.createdAt,
+      //   // updatedAt: req.body.updatedAt
+      // }
+  )
     .then(function (data) {
       res.json(data);
     });
   });
-  app.post('api/new/user', function (req, res) {
+
+  app.post('/api/new/user', function (req, res) {
     console.log(req.body);
     db.users.create({
       username: req.body.username,
