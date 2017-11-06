8880fb70dc2413ad6cc56b64c726248bcbe65614
Alison Lawyer <alison@Alisons-MacBook-Air.local>
2017-11-06 09:23:00 -0800

    began implementation of passport for user authentication

--- public/js/login.js
+++ public/js/login.js
@@ -12,8 +12,11 @@ $(document).ready(function () {
       username: $username.val().trim(),
       password: $password.val()
     };
-    // this only works if user does exist (no password validation yet, only checks username)
-    // sql throws an error if user does not exist and won't complete function
+    // this now works if user exists or does not. Now we need to implement the
+    // password functionality to make sure that entered username + password match
+
+    // not sure where in here do we refer to passport? did I put the function in
+    // the wrong file? (currently in api-routes, should it be here?)
     $.post('/api/check-user', currUser, function (res) {
       if (!res.user) {
         alert('No user exists, sign up as new user.');
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,7 @@
 
 var db = require('../models');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 module.exports = function (app) {
   app.get('/api/restrooms', function (req, res) {
@@ -23,19 +25,46 @@ module.exports = function (app) {
       res.json(data);
     });
   });
+  // not sure if this should go in this file or in login.js...
+  passport.use(new LocalStrategy(
+    function (username, password, done) {
+      db.User.findOne({
+        where: {
+          username: username
+        }
+      }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        // somewhere in here, use bcrypt to hash/unhash passwords
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+));
+  // this is right, its the post route
+  app.post('/api/check-user',
+    passport.authenticate('local', {
+      successRedirect: '/landing_list',
+      failureRedirect: '/login',
+      failureFlash: true })
+  );
 
-  app.post('/api/check-user', function (req, res) {
-    console.log(req.body);
-    db.User.findOne({
-      where: {
-        username: req.body.username
-      }
-    }).then(function (data) {
-      if (data == null) {
-        res.send({user: false});
-      } else {
-        res.send({user: true});
-      }
-    });
-  });
+  // function (req, res) {
+  //   console.log(req.body);
+  //   db.User.findOne({
+  //     where: {
+  //       username: req.body.username
+  //     }
+  //   }).then(function (data) {
+  //     if (data == null) {
+  //       res.send({user: false});
+  //     } else {
+  //       res.send({user: true});
+  //     }
+  //   });
+  // });
 };
