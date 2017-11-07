058eda9b9346abe2995d5bdcaa93ecab2e4f48eb
Maddy Ford <mrhf008@gmail.com>
2017-11-07 09:58:15 -0800

    adding more user auth stuff, nothing broken yet

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,11 @@ var db = require('../models');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
+var authController = require('../controller/authcontroller.js');
+
 module.exports = function (app) {
+  app.get('/signup', authController.signup);
+
   app.get('/api/restrooms', function (req, res) {
     db.restroom.findAll({})
     .then(function (data) {
@@ -25,33 +29,34 @@ module.exports = function (app) {
       res.json(data);
     });
   });
+
   // not sure if this should go in this file or in login.js...
-  passport.use(new LocalStrategy(
-    function (username, password, done) {
-      db.User.findOne({
-        where: {
-          username: username
-        }
-      }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        // somewhere in here, use bcrypt to hash/unhash passwords
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      });
-    }
-));
+//   passport.use(new LocalStrategy(
+//     function (username, password, done) {
+//       db.User.findOne({
+//         where: {
+//           username: username
+//         }
+//       }, function (err, user) {
+//         if (err) { return done(err); }
+//         if (!user) {
+//           return done(null, false, { message: 'Incorrect username.' });
+//         }
+//         // somewhere in here, use bcrypt to hash/unhash passwords
+//         if (!user.validPassword(password)) {
+//           return done(null, false, { message: 'Incorrect password.' });
+//         }
+//         return done(null, user);
+//       });
+//     }
+// ));
   // this is right, its the post route
-  app.post('/api/check-user',
-    passport.authenticate('local', {
-      successRedirect: '/landing_list',
-      failureRedirect: '/login',
-      failureFlash: true })
-  );
+  // app.post('/api/check-user',
+  //   passport.authenticate('local', {
+  //     successRedirect: '/landing_list',
+  //     failureRedirect: '/login',
+  //     failureFlash: true })
+  // );
 
   // function (req, res) {
   //   console.log(req.body);
